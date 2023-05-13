a6ffb05d5555243ac7991c76ad1e03e401528b77
Tari Balázs <taribalazs2020@gmail.com>
2023-05-13 13:04:53 +0200

    basic syntax

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
+#!/usr/bin/python
+
 print ("Hello, World!")
+
+# Python - Basic Syntax. (2012, June 10). Tutorials Point. Retrieved May 13, 2023, from https://www.tutorialspoint.com/python/python_basic_syntax.htm#
