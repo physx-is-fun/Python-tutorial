e7e15b88f547929f0b11c77f0d78203bef4dd2ce
Tari Balázs <taribalazs2020@gmail.com>
2023-05-14 09:44:23 +0200

    comments

---
+++ comments.py
@@ -0,0 +1,9 @@
+# This is a single line comment in python
+print ("Hello, World!") # This is again comment
+
+'''
+This is a multiline
+comment.
+'''
+
+# Python - Comments. (2012, June 10). Tutorials Point. Retrieved May 13, 2023, from https://www.tutorialspoint.com/python/python_comments.htm
